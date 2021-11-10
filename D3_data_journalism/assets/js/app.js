ff1e4a2b9025903e33dc15c8ff123c6852f7357b
Greg Behnke <gregbehnke24x7@msn.com>
2021-11-10 16:47:54 -0600

    commit progress

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -15,6 +15,95 @@ var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
 //************************************************************************
+// create svg element
+//************************************************************************
+var svg = d3.select("#scatter") 
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+//************************************************************************
+// add chart to svg element  
+//************************************************************************
+var chart = svg.append("g") 
+  .attr("height", height)
+  .attr("width", width)
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+//************************************************************************
+// set initial active attributes
+//************************************************************************
+var xattr = "poverty";
+var yattr = "obesity";
+
+//************************************************************************
+// create axis functions
+//************************************************************************
+var bottomAxis = d3.axisBottom(xLinearScale);
+var leftAxis = d3.axisLeft(yLinearScale);
+
+//************************************************************************
+// append axes to chart
+//************************************************************************
+var xAxis = chart.append("g")
+    .attr("transform", `translate(0, ${height})`)
+    .call(bottomAxis);
+  
+    chart.append("g")
+    .call(leftAxis); 
+
+//************************************************************************
+// create group for axis labels
+//************************************************************************
+var labelsGroup = chart.append("g")
+    .attr("transform", `translate(${width / 2}, ${height + 20})`);
+
+var povertyLabel = labelsGroup.append("text")
+    .attr("x", 0)
+    .attr("y", 15)
+    .attr("value", "poverty")
+    .classed("active", true)
+    .text("In Poverty %");
+
+var incomeLabel = labelsGroup.append("text")
+    .attr("x", 0)
+    .attr("y", 30)
+    .attr("value", "income")
+    .classed("inactive", true)
+    .text("Household Income (Median)");
+
+var ageLabel = labelsGroup.append("text")
+    .attr("x", 0)
+    .attr("y", 45)
+    .attr("value", "age") 
+    .classed("inactive", true)
+    .text("Age (Median)");  
+ 
+var obesityLabel = labelsGroup.append("text")
+    .attr("transform","rotate(-90)")
+    .attr("x", (margin.left) * 2.5)
+    .attr("y", 0 - (height -60))
+    .attr("value", "obesity") 
+    .classed("active", true)
+    .text("Obesity (%)");
+
+var smokesLabel = labelsGroup.append("text")
+    .attr("transform","rotate(-90)")
+    .attr("x", (margin.left) * 2.5)
+    .attr("y", 0 - (height -40))
+    .attr("value", "smokes") 
+    .classed("inactive", true)
+    .text("Smokes (%)");
+
+var healthcareLabel = labelsGroup.append("text")
+    .attr("transform","rotate(-90)")
+    .attr("x", (margin.left) * 2.5)
+    .attr("y", 0 - (height -20))
+    .attr("value", "healthcare") 
+    .classed("inactive", true)
+    .text("Lacks Healthcare (%)"); 
+    
+//************************************************************************
 // read data
 //************************************************************************
 d3.csv("assets/data/data.csv").then(function (data) {
